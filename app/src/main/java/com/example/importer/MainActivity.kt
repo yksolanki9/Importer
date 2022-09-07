dc5473faf3048f8c78d07fb42a7795477311d999
yksolanki9 <yashsolankli1709@gmail.com>
2022-09-07 20:36:28 +0530

    Minor fixes

--- app/src/main/java/com/example/importer/MainActivity.kt
+++ app/src/main/java/com/example/importer/MainActivity.kt
@@ -9,23 +9,23 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var imageView: ImageView? = null;
-    private var textContent: TextView? = null;
+    private var imageView: ImageView? = null
+    private var textContent: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textContent = findViewById(R.id.tv_heading)
-        imageView = findViewById(R.id.iv_image);
+        imageView = findViewById(R.id.iv_image)
 
         intent?.let {
             if(it.action == Intent.ACTION_SEND) {
                 it.type?.let { intentType ->
                     if(intentType.startsWith("image")) {
-                        val test = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM);
-                        imageView?.setImageURI(test);
-                        textContent?.setText("Here's your selected image");
+                        val test = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                        imageView?.setImageURI(test)
+                        textContent?.text = "Here's your selected image"
                     }
                 }
             }
