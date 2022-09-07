1c69654dffce567512f6f596b92b5713d11e8151
yksolanki9 <yashsolankli1709@gmail.com>
2022-09-07 20:37:09 +0530

    Minor

--- app/src/main/java/com/example/importer/MainActivity.kt
+++ app/src/main/java/com/example/importer/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     private var imageView: ImageView? = null
     private var textContent: TextView? = null
 
